6fea75e556db528c29007f415c724c86d22be858
Virter <nikita.andreevich.9797@mail.ru>
2026-05-02 10:11:33 +0500

    Начало работы над веб-приложнием. С помощью Spring Initializr создана структура проекта. С помощью Spring Boot создаётся страница с Hello, world!

---
+++ team_task_manager/src/main/java/com/teamtask/team_task_manager/TeamTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.teamtask.team_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTaskManagerApplication.class, args);
+	}
+
+}
